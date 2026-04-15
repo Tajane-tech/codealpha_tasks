f6723b56750df941892e3d4f61468c2445ea60c4
Tajane-tech <tanvitajane60@gmail.com>
2026-04-15 14:40:16 +0530

    Add files via upload

---
+++ task_3/car.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, r2_score
+
+df = pd.read_csv("car data.csv")
+
+df.drop(['Car_Name'], axis=1, inplace=True)
+
+df['Car_Age'] = 2025 - df['Year']
+df.drop(['Year'], axis=1, inplace=True)
+
+df = pd.get_dummies(df, drop_first=True)
+
+X = df.drop('Selling_Price', axis=1)
+y = df['Selling_Price']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Mean Absolute Error:", mean_absolute_error(y_test, y_pred))
+print("R2 Score:", r2_score(y_test, y_pred))
+
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Price")
+plt.ylabel("Predicted Price")
+plt.title("Actual vs Predicted Car Prices")
+plt.show()
